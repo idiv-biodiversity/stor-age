6951c499d231151b62ae23af7191dff6008c8d95
Christian Krause <christian.krause@idiv.de>
2019-05-13 11:59:38 +0200

    refactors cli
    use a variable for each argument, improves readability

--- src/cli.rs
+++ src/cli.rs
@@ -13,19 +13,58 @@ pub fn build(color: bool) -> App<'static, 'static> {
         AppSettings::ColorNever
     };
 
+    let age = Arg::with_name("age")
+        .help("threshold in days")
+        .required(true)
+        .validator(is_number);
+
+    let debug = Arg::with_name("debug")
+        .long("debug")
+        .help("debug output")
+        .display_order(2);
+
+    let dir = Arg::with_name("dir")
+        .help("input directories")
+        .long_help(
+"The input directories for which to gather information. If none are given, \
+ directories are read from standard input. This way, this tool can be used in \
+ pipes that get their input from e.g. `find`.",
+        )
+        .multiple(true)
+        .validator(is_dir);
+
+    let format = Arg::with_name("format")
+        .long("format")
+        .help("output format")
+        .long_help(
+"Specify output format. Pretty is intended for human-readable interactive \
+ use. Oneline is intended as machine-readable output that shows a colon \
+ (\":\") separated list of total, unaccessed, and unmodified size in bytes, \
+ followed by the directory.",
+        )
+        .takes_value(true)
+        .case_insensitive(true)
+        .possible_values(&Output::variants())
+        .default_value("Pretty");
+
+    let verbose = Arg::with_name("verbose")
+        .short("v")
+        .long("verbose")
+        .help("verbose output")
+        .display_order(2);
+
     let conditional_compilation_args: Vec<Arg> =
         vec![#[cfg(feature = "spectrum-scale")]
         Arg::with_name("spectrum-scale")
             .long("spectrum-scale")
-            .help("use mmapplypolicy")
+            .help("use mmapplypolicy instead of universal directory traversal")
             .long_help(
-                "On IBM Spectrum Scale file systems exists a dedicated \
-                 command that allows more efficient file system traversal, \
-                 called `mmapplypolicy`. Using this flag forces the usage of \
-                 this command over the universal directory traversal. At the \
-                 time of this writing, according to Spectrum Scale \
-                 documentation, only the super-user `root` may use the \
-                 `mmapplypolicy` command.",
+"On IBM Spectrum Scale file systems exists a dedicated command that allows \
+ more efficient file system traversal, called `mmapplypolicy`. Using this \
+ flag forces the usage of this command over the universal directory \
+ traversal. At the time of this writing, according to Spectrum Scale \
+ documentation, only the super-user `root` may use the `mmapplypolicy` \
+ command.",
             )
             .display_order(1)];
 
@@ -35,55 +74,11 @@ pub fn build(color: bool) -> App<'static, 'static> {
         .global_setting(color)
         .max_term_width(80)
         .help_short("?")
-        .help_message("show this help output")
-        .version_message("show version")
-        .arg(
-            Arg::with_name("age")
-                .help("threshold in days")
-                .required(true)
-                .validator(is_number),
-        )
-        .arg(
-            Arg::with_name("dir")
-                .help("input directories")
-                .long_help(
-                    "The input directories for which to gather \
-                     information. If none are given, directories are read \
-                     from standard input. This way, this tool can be used \
-                     in pipes that get their input from e.g. `find`.",
-                )
-                .multiple(true)
-                .validator(is_dir),
-        )
-        .arg(
-            Arg::with_name("debug")
-                .long("debug")
-                .help("debug output")
-                .display_order(2),
-        )
-        .arg(
-            Arg::with_name("verbose")
-                .short("v")
-                .long("verbose")
-                .help("verbose output")
-                .display_order(2),
-        )
-        .arg(
-            Arg::with_name("format")
-                .long("format")
-                .help("output format")
-                .long_help(
-                    "Specify output format. Pretty is intended for \
-                     human-readable interactive use. Oneline is intended as \
-                     machine-readable output that shows a colon (\":\") \
-                     separated list of total, unaccessed, and unmodified size in \
-                     bytes, followed by the directory.",
-                )
-                .takes_value(true)
-                .case_insensitive(true)
-                .possible_values(&Output::variants())
-                .default_value("Pretty"),
-        )
+        .arg(age)
+        .arg(dir)
+        .arg(debug)
+        .arg(verbose)
+        .arg(format)
         .args(&conditional_compilation_args)
 }
 
