8bf5df3c7994bdaf92ad643e337c9eab784837e8
Christian Krause <christian.krause@idiv.de>
2019-05-15 12:21:24 +0200

    tests cli args

---
+++ tests/cli-args.rs
@@ -0,0 +1,41 @@
+use assert_cmd::crate_name;
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::error::Error;
+use std::process::Command;
+
+#[test]
+fn arg_age_invalid() -> Result<(), Box<Error>> {
+    let mut cmd = Command::cargo_bin(crate_name!()).unwrap();
+    cmd.arg("not-an-age");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("not a number"));
+
+    Ok(())
+}
+
+#[test]
+fn arg_dir_does_not_exist() -> Result<(), Box<Error>> {
+    let mut cmd = Command::cargo_bin(crate_name!()).unwrap();
+    cmd.arg("90").arg("test/file/doesnt/exist");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("does not exist"));
+
+    Ok(())
+}
+
+#[test]
+fn arg_dir_not_a_dir() -> Result<(), Box<Error>> {
+    let mut cmd = Command::cargo_bin(crate_name!()).unwrap();
+    cmd.arg("90").arg("Cargo.toml");
+
+    cmd.assert()
+        .failure()
+        .stderr(predicate::str::contains("is not a directory"));
+
+    Ok(())
+}
